b5b7f8471cb0ea02165e9327c897e9f8167ebeef
Trịnh Hiệp <90686182+hieptrinh2002@users.noreply.github.com>
2023-12-06 20:53:16 +0700

    reset migration

--- BHYT.API/Migrations/20231206135056_init.Designer.cs
+++ BHYT.API/Migrations/20231206135056_init.Designer.cs
@@ -4,6 +4,7 @@ using BHYT.API.Models.DbModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BHYT.API.Migrations
 {
     [DbContext(typeof(BHYTDbContext))]
-    partial class BHYTDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231206135056_init")]
+    partial class init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
